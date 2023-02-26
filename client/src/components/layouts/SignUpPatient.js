aa3710a2dc7b26240d709d174adf579e84741db5
Harsh Kushwaha <harshkush072001@gmail.com>
2023-02-26 23:32:50 +0530

    completed signup form patient

--- client/src/components/layouts/SignUpPatient.js
+++ client/src/components/layouts/SignUpPatient.js
@@ -1,7 +1,35 @@
 import React,{useState,useContext} from 'react'
+import {useNavigate } from "react-router-dom";
+import AuthContext from "../../context/auth/AuthContext";
 
 const SignUpPatient = () => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenicated, error, register, loading, loadUser } = authContext;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    roll_number: "",
+    phone: "",
+    gender: "male",
+    password: "",
+    cnf_password: "",
+    birth: "",
+  });
+  const [formLoad, setFormLoad] = useState(loading);
+  const [err, setErr] = useState({
+    type: null,
+    msg: null,
+  });
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+    register(form); 
+   };
   return (
     <div class='container-xl px-4'>
     <h3 className='text-center'>Create New account</h3>
@@ -12,51 +40,102 @@ const SignUpPatient = () => {
           <div class='card-body'>
             <form>
               <div className='mb-3'>
-              <label class='small mb-1' for='inputemail'>
-                    Email address
-              </label>
-              <input
-                    // onChange={onChange}
-                    required={true}
-                    name='email'
-                    class='form-control'
-                    id='inputemail'
-                    type='email'
-                    placeholder='Your e-mail'
-                  />
-              </div>
-              <div class='mb-3'>
                 <label class='small mb-1' for='inputemail'>
-                  Email address
+                      Email address
                 </label>
                 <input
-                  required={true}
-                  name='email'
-                  class='form-control'
-                  id='inputemail'
-                  type='email'
-                  placeholder="Enter actor's e-mail"
-                />
+                      onChange={onChange}
+                      required={true}
+                      name='email'
+                      class='form-control'
+                      id='inputemail'
+                      type='email'
+                      placeholder='Your e-mail'
+                    />
               </div>
-              <div class='mb-3'>
-                <label class='small mb-1' for='inputName'>
-                  Name
-                </label>
-                <input
-                  required={true}
-                  name='name'
-                  class='form-control'
-                  id='inputName'
-                  type='text'
-                  placeholder="Enter actor's Full name"
-                />
-              </div>
+              <div class='row gx-6 mb-3'>
+                  <div class='col-md-6'>
+                    <label class='small mb-1' for='inputRollno'>
+                      Rollno
+                    </label>
+                    <input
+                      onChange={onChange}
+                      required={true}
+                      name='roll_number'
+                      class='form-control'
+                      id='inputRollno'
+                      type='text'
+                      placeholder='Enter your Rollno'
+                    />
+                  </div>
+                  <div class='col-md-6'>
+                    <label class='small mb-1' for='inputName'>
+                      Name
+                    </label>
+                    <input
+                      onChange={onChange}
+                      required={true}
+                      name='name'
+                      class='form-control'
+                      id='inputName'
+                      type='text'
+                      placeholder='Enter your Name'
+                    />
+                  </div>
+                </div>
+                <div class='row gx-3 mt-3 mb-3'>
+                  <div class='col-md-2'>
+                    <label class='small mb-1' for='inputGender'>
+                      Gender
+                    </label>
+                    <select
+                      onChange={onChange}
+                      required={true}
+                      name='gender'
+                      class='form-select'
+                      aria-label='Select Your Gender'
+                    >
+                      <option>Male</option>
+                      <option>Female</option>
+                      <option>Other</option>
+                    </select>
+                  </div>
+                  <div class='col-md-4'>
+                    <label class='small mb-1' for='inputBirthday'>
+                      Birthday
+                    </label>
+                    <input
+                      onChange={onChange}
+                      required={true}
+                      name='birth'
+                      class='form-control'
+                      id='inputBirthday'
+                      type='date'
+                      placeholder='Enter your birth date'
+                    />
+                  </div>
+                  <div class='col-md-6'>
+                    <label class='small mb-1' for='inputPhone'>
+                      Phone number
+                    </label>
+                    <input
+                      onChange={onChange}
+                      required={true}
+                      name='phone'
+                      class='form-control'
+                      id='inputPhone'
+                      type='integer'
+                      placeholder='Enter your phone number'
+                    />
+                  </div>
+                </div>
               <div class='row gx-3 mt-3 mb-3'>
                 <label class='small mb-1' for='inputAddress'>
                   Set Password
                 </label>
                 <div class='col-md-6'>
                   <input
+                    onChange={onChange}
                     required={true}
                     name='password'
                     type='password'
@@ -68,6 +147,7 @@ const SignUpPatient = () => {
                 </div>
                 <div class='col-md-6'>
                   <input
+                    onChange={onChange}
                     required={true}
                     name='cnf_password'
                     type='password'
@@ -80,9 +160,10 @@ const SignUpPatient = () => {
               </div>
               <div class='d-grid gap-2 mt-5'>
                 <button
+                  onClick={onSubmit}
                   class='btn btn-primary'
                   type='button'
-                >
+                 >
                   Submit
                 </button>
               </div>
