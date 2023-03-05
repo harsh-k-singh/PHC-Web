474ee56ff0b519850e67341342f6eb052310152b
Harsh Kushwaha <harshkush072001@gmail.com>
2023-03-05 09:18:49 +0530

    removed extra file

--- client/src/components/layouts/CheckDoctorsSchedule.js
+++ client/src/components/layouts/CheckDoctorsSchedule.js
@@ -4,10 +4,7 @@ import GlobalContext from "../../context/global/GlobalContext";
 
 const CheckDoctorsSchedule = () => {
   const globalContext = useContext(GlobalContext);
-  const {
-    getDoctorSchedule,
-    doctorSchedule,
-  } = globalContext;
+  const { getDoctorSchedule, doctorSchedule } = globalContext;
   const [data, setData] = useState(null);
   const d = new Date().getDay();
   const [day, setDay] = useState(d);
@@ -18,77 +15,77 @@ const CheckDoctorsSchedule = () => {
     };
     fetchData();
     setDay(day);
-  }, [day]);
+  }, [day,data]);
   return (
-    <div className='container mt-4'>
-      <div class='card text-center'>
-        <div class='card-header'>
-          <ul class='nav nav-tabs card-header-tabs'>
-            <li class='nav-item'>
+    <div className="container mt-4">
+      <div class="card text-center">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 1 ? "active" : ""}`}
                 aria-current={day === 1 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(1)}
               >
                 Monday
               </a>
             </li>
-            <li class='nav-item'>
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 2 ? "active" : ""}`}
                 aria-current={day === 2 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(2)}
               >
                 Tuesday
               </a>
             </li>
-            <li class='nav-item'>
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 3 ? "active" : ""}`}
                 aria-current={day === 3 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(3)}
               >
                 Wednesday
               </a>
             </li>
-            <li class='nav-item'>
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 4 ? "active" : ""}`}
                 aria-current={day === 4 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(4)}
               >
                 Thursday
               </a>
             </li>
-            <li class='nav-item'>
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 5 ? "active" : ""}`}
                 aria-current={day === 5 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(5)}
               >
                 Friday
               </a>
             </li>
-            <li class='nav-item'>
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 6 ? "active" : ""}`}
                 aria-current={day === 6 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(6)}
               >
                 Saturday
               </a>
             </li>
-            <li class='nav-item'>
+            <li class="nav-item">
               <a
                 class={`nav-link ${day === 0 ? "active" : ""}`}
                 aria-current={day === 0 ? "true" : "false"}
-                href='#'
+                href="#"
                 onClick={() => setDay(0)}
               >
                 Sunday
@@ -96,18 +93,18 @@ const CheckDoctorsSchedule = () => {
             </li>
           </ul>
         </div>
-        <div class='card-body'>
+        <div class="card-body">
           <table
-            class='table my-3 text-center'
+            class="table my-3 text-center"
             style={{ width: "80%", margin: "auto" }}
           >
-            <thead class='table-dark'>
+            <thead class="table-dark">
               <tr>
-                <th scope='col'>S.No</th>
-                <th scope='col'>Name</th>
-                <th scope='col'>Suffix</th>
-                <th scope='col'>Arrival Time</th>
-                <th scope='col'>Departure Time</th>
+                <th scope="col">S.No</th>
+                <th scope="col">Name</th>
+                <th scope="col">Suffix</th>
+                <th scope="col">Arrival Time</th>
+                <th scope="col">Departure Time</th>
               </tr>
             </thead>
             <tbody>
@@ -115,7 +112,7 @@ const CheckDoctorsSchedule = () => {
                 ? data.map((item, index) => {
                     return (
                       <tr>
-                        <th scope='row'>{index + 1}</th>
+                        <th scope="row">{index + 1}</th>
                         <td>{item.name}</td>
                         <td>{item.degree}</td>
                         <td>{item.timing[day][0]}</td>
